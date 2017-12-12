6c24346541800c6b12dfa905f14c2e5ae9d71f75
meditationincolorado <media@meditationincolorado.org>
2017-12-12 12:38:49 -0700

    KL Main Logo @media Fixes
    changed logo so that full logo displays on desktop/iPad, small logo displays on phone

--- nav.php
+++ nav.php
@@ -1,6 +1,9 @@
 <nav id="main-nav" class="row"> <!--banner-cta-->
 	<div class="col-xs-12 col-sm-10 col-sm-offset-1">
 		<a href="<?php echo get_home_url(); ?>" id="logo">
+			<?php include('kmcco_svg.php'); ?>
+		</a>
+		<a href="<?php echo get_home_url(); ?>" id="big-logo">
 			<?php include('kmcco_full_svg.php'); ?>
 		</a>
 			<?php
