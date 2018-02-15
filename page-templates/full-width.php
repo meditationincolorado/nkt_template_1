7199bbda744ca19f62cc5146d395b0f77564c835
meditationincolorado <media@meditationincolorado.org>
2018-02-15 13:20:42 -0700

    Enhanced Full-Width Template
    Pulled code from subpage template to insert into full-width template

--- page-templates/full-width.php
+++ page-templates/full-width.php
@@ -9,6 +9,31 @@
 
 get_header(); ?>
 
+<?php include(get_template_directory() . '/nav.php'); ?>
+
+<?php $google_api_key = 'AIzaSyAnL4zStsZJPP02QcauWi52QUHUReLg9UE'; ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post();
+	$title = get_the_title();
+	$thumb_id = get_post_thumbnail_id();
+ 	$isDefined = is_int($thumb_id); 
+	$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+	$thumb_url = $thumb_url_array[0];	
+?>
+
+	<div id="subpage-hero" class="row cover center_vert_parent" style='background-image: url("<?php echo $thumb_url; ?>");'>
+		<div class="tint"></div>
+			<hgroup class="col-xs-12 col-xs-10 col-xs-offset-1 center_vert">
+				<h1><?php echo get_the_title() ?></h1>
+				<p><?php echo $post->subpage_blurb; ?></p>
+			</hgroup>
+		<?php include(get_template_directory() . '/mobile-nav.php'); ?>
+	</div>
+
+	<!-- Subnav -->
+	<?php include(get_template_directory() . '/subpage_subnav.php'); ?>
+
+	<!-- Content -->
 <div id="main-content" class="main-content">
 
 <?php
