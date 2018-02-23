be56f614a206c75aa4196ad0148601449a90a618
meditationincolorado <media@meditationincolorado.org>
2018-02-23 14:07:17 -0700

    Nav/Google Cal Separate Page Implentation
    Changed the nav to be non-hard code customizable (directly accessible through WP), removed Google Cal from hard code so that it can be implemented on its own page

--- nav.php
+++ nav.php
@@ -6,25 +6,8 @@
 		<a href="<?php echo get_home_url(); ?>" id="big-logo">
 			<?php include('kmcco_full_svg.php'); ?>
 		</a>
-			<?php
-				$args1 = array( 
-					'post_type' => 'page',
-					'posts_per_page' => 8,
-					'meta_key' => 'nav_order',
-					'orderby' => 'meta_value_num',
-					'order'   => 'ASC'
-			    );
-
-		        $navSubpages1 = new WP_Query($args1);
-			?>
-		<ul>
-			<?php
-		        while ($navSubpages1->have_posts()) : $navSubpages1->the_post(); ?>
-
-					<li><a href="<?php echo get_permalink( $post->ID ) ?>"><?php echo get_the_title();?></a></li>
-		        <?php endwhile;
-		        wp_reset_postdata();
-			?>
-		</ul>
+		<?php wp_nav_menu( array(
+		'menu_id'        => 'primary-menu',
+	) ); ?>
 	</div>
 </nav>
