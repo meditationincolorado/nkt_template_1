b839eb70d13a7d38f4dcffddaca30bbafd1c54bd
meditationincolorado <media@meditationincolorado.org>
2018-02-25 14:37:37 -0700

    Undo Mobile Nav Try 2 Changes

--- mobile-nav.php
+++ mobile-nav.php
@@ -1,7 +1,30 @@
 <nav id="mobile-nav">
 
 
-		<?php wp_nav_menu( array(
-		'menu_id'        => 'primary-menu',
-	) ); ?>
+	<?php
+		/*$args1 = array(
+            'post_type' => 'page',
+            'meta_key' => '_wp_page_template',
+            'meta_value' => 'page-templates/subpage.php',
+			'orderby' => 'sort',
+			'order'   => 'ASC',
+   			'posts_per_page' => 3
+          );*/
+    $args1 = array( 
+      'post_type' => 'page',
+      'posts_per_page' => 4,
+      'meta_key' => 'nav_order',
+      'orderby' => 'meta_value_num',
+      'order'   => 'ASC'
+    );
+
+
+		
+        $subNavSubpages = new WP_Query($args1);
+
+        while ($subNavSubpages->have_posts()) : $subNavSubpages->the_post(); ?>
+			<a href="<?php echo get_permalink( $post->ID ) ?>"><?php echo get_the_title();?></a>
+        <?php endwhile;
+        wp_reset_postdata();
+	?>
 </nav>
