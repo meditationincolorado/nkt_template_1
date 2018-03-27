090f05c58dbd56407f79cbc4c89e636412e2192f
meditationincolorado <media@meditationincolorado.org>
2018-03-27 13:08:46 -0600

    Tab Implementation on Posts
    Made big changes to Single.php to make tabs and subpage styling functional on individual posts, and made some style.css changes to accommodate

--- single.php
+++ single.php
@@ -12,6 +12,8 @@
 get_header(); ?>
 <?php include(get_template_directory() . '/nav.php'); ?>
 
+<?php $google_api_key = 'AIzaSyAnL4zStsZJPP02QcauWi52QUHUReLg9UE'; ?>
+
 <?php if (have_posts()) : while (have_posts()) : the_post();
 	$thumb_id = get_post_thumbnail_id();
  	$isDefined = is_int($thumb_id); 
@@ -25,11 +27,23 @@ get_header(); ?>
 				<h1><?php echo get_the_title(); ?></h1>
 				<p><?php echo $post->subpage_blurb; ?></p>
 			</hgroup>
+		<?php include(get_template_directory() . '/mobile-nav.php'); ?>
 	</div>
 
+	<!-- Content -->
+
 	<div id="content" class="row single-php">
+	<?php 
+    			$bkgColor = ($post->feature_section_background_color) ? 'background-color: ' . $post->feature_section_background_color . '; ' : '';
+    			$fontColor = ($post->font_color) ? 'color: ' . $post->font_color . '; ': '';
+    			$style = 'style="' . $bkgColor . $fontColor . '"';
+    		?>
+	<section class="nav-section" <?php echo $style; ?>>
 		<div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
-			<?php the_content(); ?>
+		<article id="<?php echo $anchorTag; ?>">
+	        		<a name="<?php echo $anchorTag; ?>"></a>
+		        	<h2><?php echo get_the_title(); ?></h2>
+					<?php the_content(); ?>
 
 			<!-- <?php
 				$args = array( 
@@ -49,8 +63,10 @@ get_header(); ?>
 		        <?php endwhile;
 		        wp_reset_postdata();
 			?> -->
+			</article>
 		</div>
-	</div>
+		<div style="clear:both"></div>
+		</section>
 <?php endwhile; endif; ?>
 
 
