a722b3b0512d1b118e6e9477bb507ac698bf552e
meditationincolorado <media@meditationincolorado.org>
2018-05-29 09:19:55 -0600

    Putting Logo back to OG

--- nav.php
+++ nav.php
@@ -1,13 +1,10 @@
 <nav id="main-nav" class="row"> <!--banner-cta-->
+	<?php wp_nav_menu( array(
+		'menu_id'        => 'primary-menu',
+	) ); ?>
 	<div class="col-xs-12 col-sm-10 col-sm-offset-1">
 		<a href="<?php echo get_home_url(); ?>" id="logo">
 			<?php include('kmcco_svg.php'); ?>
 		</a>
-		<a href="<?php echo get_home_url(); ?>" id="big-logo">
-			<?php include('kmcco_full_svg.php'); ?>
-		</a>
-		<?php wp_nav_menu( array(
-		'menu_id'        => 'primary-menu',
-	) ); ?>
 	</div>
 </nav>
